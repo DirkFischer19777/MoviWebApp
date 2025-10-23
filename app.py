3a644a35c25c4535e30ca58e109bf1f5eb8e43a5
dirkfischer <fischertechnic@gmx.net>
2025-10-23 12:50:48 +0200

    feat: add full route roadmap and base Flask routes for users and movies

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
 from data_manager import DataManager
-from models import db, Movie
+from models import db
 import os
 
 # -------------------------------------------
@@ -28,8 +28,48 @@ data_manager = DataManager()
 
 @app.route('/')
 def home():
-    """Simple test route to verify app is running"""
-    return "Welcome to MoviWeb App!"
+    """Homepage showing all users"""
+    users = data_manager.get_users()
+    return render_template('index.html', users=users)
+
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    """Add a new user"""
+    name = request.form.get('name')
+    data_manager.create_user(name)
+    return redirect(url_for('home'))
+
+
+@app.route('/users/<int:user_id>/movies', methods=['GET'])
+def user_movies(user_id):
+    """List all movies for a specific user"""
+    movies = data_manager.get_movies(user_id)
+    return render_template('movies.html', movies=movies, user_id=user_id)
+
+
+@app.route('/users/<int:user_id>/movies', methods=['POST'])
+def add_movie(user_id):
+    """Add a movie to user's list"""
+    title = request.form.get('title')
+    data_manager.add_movie(user_id, title)
+    return redirect(url_for('user_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
+def update_movie(user_id, movie_id):
+    """Update a movie title"""
+    new_title = request.form.get('new_title')
+    data_manager.update_movie(movie_id, new_title)
+    return redirect(url_for('user_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    """Delete a movie"""
+    data_manager.delete_movie(movie_id)
+    return redirect(url_for('user_movies', user_id=user_id))
+
 
 
 # -------------------------------------------
